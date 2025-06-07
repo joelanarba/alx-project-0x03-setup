a8f4e6ddf73a4d4b831f64e20df8247dd5a6529a
Joel Anarba <157653692+joelanarba@users.noreply.github.com>
2025-06-07 22:39:42 +0000

    Imported Montserrat Google font and applied globally

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,11 +1,9 @@
-import Layout from "@/components/layouts/Layout";
-
-export default function Home() {
+const Home = () => {
   return (
-    <Layout>
-      <div className="min-h-screen flex items-center justify-center">
-        <h1 className="text-4xl font-bold text-center">Welcome to Splash App</h1>
-      </div>
-    </Layout>
+    <div>
+      <h1> Welcome to Splash App </h1>
+    </div>
   );
-}
+};
+
+export default Home;
